faee29da4cdc43c10c0b7a7fad200a6887308975
elikeyz <koppter.kom@gmail.com>
2020-02-29 00:32:26 +0000

    Deploying to gh-pages from  - 2d6055100ed046e56cebf41efcceedc0f5b1976c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bdcd9f90071e4ebe64c6720a85e4f65.js"
+  "/precache-manifest.1f08ea52ccd79ffb1a0404d917f60245.js"
 );
 
 self.addEventListener('message', (event) => {
